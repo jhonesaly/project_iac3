a011000f45146d5e36a069b51d3687e477fcf7bd
Jhones <jhonesaly@gmail.com>
2023-02-19 09:09:53 -0300

    Add new_iac_3.sh

---
+++ advanced/new_iac_3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## 0 - Saudação
+
+printf "\nIniciando protocolo new_iac_3...\n"
+printf "\nEm caso de dúvida, consulte a documentação disponível em <https://github.com/jhonesaly/project_iac3>\n"
+
+question_number=1
+
