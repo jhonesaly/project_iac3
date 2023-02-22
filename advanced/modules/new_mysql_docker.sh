33cf536a7352f20637da19e646eddc2497fd075d
Jhones <jhonesaly@gmail.com>
2023-02-22 15:24:05 -0300

    Bug fix

--- advanced/modules/new_mysql_docker.sh
+++ advanced/modules/new_mysql_docker.sh
@@ -40,8 +40,8 @@ sleep 30
 MYSQL_CONTAINER_ID=$(docker ps --filter "name=advanced_db_1" --format "{{.ID}}")
 printf "\nO ID do Contêiner é : $MYSQL_CONTAINER_ID\n"
 
-
 ## Inicia um shell dentro do container do MySQL
 printf "\nAplicando o script SQL...\n"
-docker exec -i $MYSQL_CONTAINER_ID sh -c "exec mysql -u root -p'$root_pass' $db_name < /disk2/publica/project_iac3/advanced/modules/dbscript.sql"
+docker cp /disk2/publica/project_iac3/advanced/modules/dbscript.sql $MYSQL_CONTAINER_ID:/dbscript.sql
+docker exec -i $MYSQL_CONTAINER_ID sh -c "exec mysql -u root -p'$root_pass' $db_name < /dbscript.sql"
 
