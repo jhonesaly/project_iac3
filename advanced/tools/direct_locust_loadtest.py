96b703684bcb24f8af6713763aff378b17a38f25
Jhones <jhonesaly@gmail.com>
2023-02-23 17:59:07 -0300

    Update direct_locust_loadtest.py

--- advanced/tools/direct_locust_loadtest.py
+++ advanced/tools/direct_locust_loadtest.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import pkgutil
 from locust import HttpUser, task, between
-from mysql.connector import connect, Error
+import mysql.connector
 
 os.system('cls')
 
@@ -14,29 +14,30 @@ for lib in need_libraries:
         print(f"A biblioteca {lib} não está instalada. Instalando...")
         subprocess.run(['pip', 'install', lib])
 
+
+# Cria o teste
+
+def mysql_test():
+    cnx = mysql.connector.connect(user='root', password='123', host='192.168.0.9', database='test1', port=3306)
+    cursor = cnx.cursor()
+    cursor.execute("SELECT preco FROM estoque WHERE id_codigo_barras = %s", ("3756392598566",))
+    result = cursor.fetchone()
+    price = result[0]
+    # Imprime o preço do produto retornado pelo banco de dados
+    print("Preço do produto: {}".format(price))
+    cursor.close()
+    cnx.close()
+
+
+
 # Cria bot de teste
 class MyUser(HttpUser):
-    wait_time = between(1, 5)
+    wait_time = between(1, 2)
 
-    @task(1)
-    def get_price(self):
-        try:
-            # Estabelece conexão com o banco de dados MySQL
-            with connect(
-                host="192.168.0.9",
-                user="root",
-                password="123",
-                database="test1",
-                port=3306
-            ) as connection:
-                with connection.cursor() as cursor:
-                    # Executa uma query que busca o preço do produto com o código de barras especificado
-                    cursor.execute("SELECT preco FROM estoque WHERE id_codigo_barras = %s", ("3756392598566",))
-                    result = cursor.fetchone()
-                    price = result[0]
-                    # Imprime o preço do produto retornado pelo banco de dados
-                    print("Preço do produto: {}".format(price))
-        except Error as e:
-            print(e)
+    @task
+    def mysql_task(self):
+        mysql_test()
 
-# Para abrir a interface use o comando: > locust -f advanced/tools/direct_locust_loadtest.py --host=http://localhost:8080
+
+# Para rodar a interface use o comando: > locust -f advanced/tools/direct_locust_loadtest.py --headless -u 10 -r 1 -t 30s --host=192.168.0.9:3306
+# Para acessá-la abra no browser o endereço: http://localhost:8089
