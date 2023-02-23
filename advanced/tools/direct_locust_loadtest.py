17f3bb445e8a3462d64c555f36444044d3a0cc3a
Jhones <jhonesaly@gmail.com>
2023-02-23 18:55:07 -0300

    Update direct_locust_loadtest.py

--- advanced/tools/direct_locust_loadtest.py
+++ advanced/tools/direct_locust_loadtest.py
@@ -15,12 +15,9 @@ for lib in need_libraries:
         print(f"A biblioteca {lib} não está instalada. Instalando...")
         subprocess.run(['pip', 'install', lib])
 
-# Define o intervalo entre as medições
-events.init(tick=0.01)
 
 # Cria o teste
 def mysql_test():
-    start_time = time.time()
     with mysql.connector.connect(user='root', password='123', host='192.168.0.9', database='test1', port=3306) as cnx:
         cursor = cnx.cursor()
         cursor.execute("SELECT preco FROM estoque WHERE id_codigo_barras = %s", ("3756392598566",))
@@ -30,8 +27,20 @@ def mysql_test():
         print("Preço do produto: {}".format(price))
         cursor.close()
         cnx.close()
+
+        
         # Envia o evento
         events.request_success.fire(request_type="MySQL", name="execute id_codigo_barras", response_time=int(time.time() * 1000), response_length=0)
+
+# Reporta o tempo de resposta
+response_time = (time.time() - start_time) * 1000
+events.request_success.fire(
+    request_type="MySQL",
+    name="execute id_codigo_barras",
+    response_time=response_time,
+    response_length=0,
+)
+
 
 # Cria bot de teste
 class MyUser(User):
@@ -40,8 +49,8 @@ class MyUser(User):
     @task
     def mysql_task(self):
         mysql_test()
+# Define o intervalo entre as medições
+events.init(tick=lambda: 10)
 
-
-
-# Para rodar a interface use o comando: > locust -f advanced/tools/direct_locust_loadtest.py --headless -u 1000 -r 100 -t 5min --host=192.168.0.9:3306
+# Para rodar a interface use o comando: > locust -f advanced/tools/direct_locust_loadtest.py --headless -u 10 -r 1 -t 30s --host=192.168.0.9:3306
 # Para acessá-la abra no browser o endereço: http://localhost:8089
