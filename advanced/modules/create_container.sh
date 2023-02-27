7263f8dbc5c0c34b1d43905be0c76ad50e0d0a4f
Jhones <jhonesaly@gmail.com>
2023-02-27 12:17:29 -0300

    Update project_iac3

--- advanced/modules/create_container.sh
+++ advanced/modules/create_container.sh
@@ -11,25 +11,20 @@ docker pull mysql
 
 printf "\nCriando serviço do MySQL...\n"
 
-docker volume create dbdata
+service_volume=dbdata
+
+docker volume create $service_volume
 
 # Script para sincronizar esse volume via NFS
+
+printf "\nIniciando o cluster...\n"
 
 docker swarm init
 
 printf "\nCriando serviço do MySQL...\n"
 
-docker service create /
-/--name mysqldb /
-/-e MYSQL_DATABASE: $db_name/
-/-e MYSQL_PASSWORD: $db_pass/
-/-e MYSQL_ROOT_PASSWORD: $root_pass/
-/-e MYSQL_USER: $root_name/
-/--restart: always/
-/-u root/
-/--mount type=volume,src=dbdata,dst=/var/lib/mysql/
-/--replicas $n_cont /
-/-p 3306:3306 mysql
+# docker service create --name mysqldb --replicas 2 --env MYSQL_DATABASE=test1 --env MYSQL_PASSWORD=123 --env MYSQL_ROOT_PASSWORD=123 --env MYSQL_USER=tester --mount type=volume,src=dbdata,dst=/var/lib/mysql -p 3306:3306 mysql:latest
+docker service create --name mysqldb --replicas $n_cont --env MYSQL_DATABASE=$db_name --env MYSQL_PASSWORD=$db_pass --env MYSQL_ROOT_PASSWORD=$root_pass --env MYSQL_USER=$root_name --mount type=volume,src=$service_volume,dst=/var/lib/mysql -p 3306:3306 mysql:latest
 
 # echo 'version: "3.0"' > docker-compose.yml
 # echo 'services:' >> docker-compose.yml
@@ -46,7 +41,6 @@ docker service create /
 # echo '    volumes:' >> docker-compose.yml
 # echo '      - ./dbdata:/var/lib/mysql' >> docker-compose.yml
 
-
 # docker-compose up -d
 
 sleep 30
@@ -57,5 +51,8 @@ printf "\nO ID do Contêiner é : $MYSQL_CONTAINER_ID\n"
 
 ## Inicia um shell dentro do container do MySQL
 printf "\nAplicando o script SQL...\n"
-docker cp /disk2/publica/project_iac3/advanced/modules/dbscript.sql $MYSQL_CONTAINER_ID:/dbscript.sql
+
+# docker cp /disk2/publica/project_iac3/advanced/modules/dbscript.sql $MYSQL_CONTAINER_ID:/dbscript.sql
+cp /disk2/publica/project_iac3/advanced/modules/dbscript.sql /var/lib/docker/volumes/dbdata/_data
+
 docker exec -i $MYSQL_CONTAINER_ID sh -c "exec mysql -u root -p'$db_pass' $db_name < /dbscript.sql"
