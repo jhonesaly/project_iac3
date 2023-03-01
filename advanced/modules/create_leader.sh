83fe51b07e6c32674f7ad3eb4590764846fabc51
Jhones <jhonesaly@gmail.com>
2023-03-01 14:54:43 -0300

    correcting minor bugs

--- advanced/modules/create_leader.sh
+++ advanced/modules/create_leader.sh
@@ -56,46 +56,49 @@ printf "\n${GREEN}Criando container do mysql_db...${NC}\n"
         --network=cluster_network \
         mysql
     mysql_container_id=$(docker ps --filter "name=mysql_db" --format "{{.ID}}")
-    printf "\nO ID do container é: $mysql_container_id\n"
     sleep 30
+
+    printf "\nO ID do container é: $mysql_container_id\n"
 
 printf "\n${GREEN}Aplicando o script SQL ao banco de dados...${NC}\n"
 
-    cp modules/database/* /var/lib/docker/volumes/db_volume/_data
+    cp -r modules/database/* /var/lib/docker/volumes/db_volume/_data
     docker exec -i $mysql_container_id sh -c "exec mysql -u root -p'$root_pass' $db_name < /var/lib/mysql/dbscript.sql"
 
 printf "\n${GREEN}Criando container do python_app...${NC}\n"
 
-    docker run -d --name python_app \
-        -v app_volume: \
+    docker run -dti --name python_app \
+        -v app_volume \
         --restart=always \
         --network=cluster_network \
         python
     python_container_id=$(docker ps --filter "name=python_app" --format "{{.ID}}")
-    printf "\nO ID do container é: $python_container_id\n"
     sleep 30
 
+    printf "\nO ID do container é: $python_container_id\n"
+    
 printf "\n${GREEN}Criando aplicação no container...${NC}\n"
 
-    cp modules/app/* /var/lib/docker/volumes/app_volume/_data
+    cp -r modules/app/* /var/lib/docker/volumes/app_volume/_data
 
 printf "\n${GREEN}Compartilhando volume do app via NFS...${NC}\n"
 
-    cp app/* /var/lib/docker/volumes/app_volume/_data
+    cp -r modules/app/* /var/lib/docker/volumes/app_volume/_data
     echo "/var/lib/docker/volumes/app_volume/_data *(rw,sync,subtree_check)" >> /etc/exports
     exportfs -ar
     systemctl restart nfs-kernel-server
 
 printf "\n${GREEN}Criando proxy...${NC}\n"
     
-    cd /var/lib/docker/volumes/proxy_volume/_data || return
+    cd modules/proxy || return
     master_ip=$(hostname -I | awk '{print $1}')      
     sed -i "/upstream all/a\        server $master_ip:80;" nginx.conf
     cp nginx.conf /var/lib/docker/volumes/proxy_volume/_data
     docker build -t nginx_configured .
     cd - || return
     docker run --name nginx_proxy -dti \
-        -v proxy_volume: \
+        -v proxy_volume \
+        --restart=always \
         --network=cluster_network \
         -p 4500:4500 nginx_configured
     sleep 30
@@ -115,13 +118,12 @@ printf "\n${GREEN}Criando proxy...${NC}\n"
     #             new_worker_id=$(docker node ls -q | xargs docker node inspect -f '{{ .ID }}' | head -n 1)
     #             new_worker_ip=$(docker node inspect --format '{{ .Status.Addr }}' $new_worker_id)
 
-    #             cd modules/proxy || return
+    #             cd /var/lib/docker/volumes/proxy_volume/_data || return
     #             sed -i "/upstream all/a\        server $new_worker_ip:80;" nginx.conf
     #             docker build -t nginx_configured .
     #             docker run --name nginx_proxy -dti --network=cluster_network --mount type=volume,src=proxy_volume,dst=/ -p 4500:4500 nginx_configured
     #             last_num_workers=num_workers
-    #             cd ..
-    #             cd ..
+    #             cd - || return
     #         continue
     #     fi
 
